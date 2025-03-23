2afbcb34329ba5b6736d9796cb71848f66b7a5af
Vincentxusizhi <117627536+Vincentxusizhi@users.noreply.github.com>
2025-03-23 19:57:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ yesButton.addEventListener("click", function() {
     document.body.innerHTML = `
         <div class="yes-screen">
             <h1 class="yes-text">!!!贴贴!! ( >᎑<)♡︎ᐝ</h1>
-            <img src="Image/5413.png" alt="拥抱" class="yes-image">
+            <img src="Image/5413.PNG" alt="拥抱" class="yes-image">
         </div>
     `;
 
